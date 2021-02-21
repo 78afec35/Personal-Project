b46d92d4d84762c8a96481469252d7208ff3544f
78afec35 <SSyuleyman@academytrainee.com>
2021-02-21 21:42:17 +0000

    basic website structure

--- personalproject.py
+++ personalproject.py
@@ -5,14 +5,16 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
-@app.route("/login", method=["POST","GET"])
+@app.route("/blog")
+def blog():
+    return render_template("blog.html")
+@app.route("/")
+def contactme():
+    return render_template("contactme.html")
+@app.route("/")
 def login():
     return render_template("login.html")
-@app.route("/<usr>")
-def user(usr):
-    return "<h1>{usr}</h1>"
-
 
 
 if __name__ == "__main__":
-        app.run(debut=True)
+    app.run()
