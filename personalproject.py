49b94a3109e298c88e1f5f70a5a34301d76087c0
78afec35 <SSyuleyman@academytrainee.com>
2021-03-05 09:31:30 +0000

    basic styling

--- personalproject.py
+++ personalproject.py
@@ -2,12 +2,38 @@ from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Suner Syuleyman',
+        'title': 'Blog Post 1',
+        'content': 'Hey this is my blog post',
+        'date_posted' : 'April 1st'
+    },
+    {
+        'author': 'Suner Syuleyman',
+        'title': 'Blog Post 2',
+        'content': 'Hey this is my second blog post',
+        'date_posted' : 'April 1st'
+    }
+
+]
+
+me = [
+    {
+        'author': 'Suner Syuleyman',
+        'title': 'Intro Page',
+        'content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'date_posted' : 'April 1st'
+    },
+
+]
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html",me=me)
 @app.route("/blog")
 def blog():
-    return render_template("blog.html")
+    return render_template("blog.html",posts=posts)
 @app.route("/contactme")
 def contactme():
     return render_template("contactme.html")
